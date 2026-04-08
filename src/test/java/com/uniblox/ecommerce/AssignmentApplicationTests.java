b17248822481450296ec712a75a656521d3f45a8
venkata pruthvi sai gunturu <gunturupruthvi27@gmail.com>
2026-04-09 02:06:42 +0530

    Initial Boiler Plate

---
+++ src/test/java/com/uniblox/ecommerce/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniblox.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
